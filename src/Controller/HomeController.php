f02dc83c550d42277ce0d2df4b169cd5704e7414
Alerte Benjalin <benjamin.alrt@gmail.com>
2019-10-24 12:40:46 +0000

    (add) Controller - Multiroutage dans controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,15 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller {
+    /**
+     * @Route("/hello/{prenom}/age/{age}", name="hello")
+     * @Route("/hello")
+     * @Route("/hello/{prenom}")
+     * Montre la page qui dit bonjour
+     */
+    public function hello($prenom = "anonyme", $age = 0){
+        return new Response('Bonjour '.$prenom. " vous avez ". $age ." ans.");
+    }
 
     /**
      * @Route("/", name="homepage")
